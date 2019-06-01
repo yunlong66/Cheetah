463ba33b84f56de85849d14394523cff1ada776e
Yan Yunlong <18810208912@163.com>
2019-06-02 02:40:42 +0800

    6.1

---
+++ JunitTest/src/TestSuite.java
@@ -0,0 +1,7 @@
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({Testcase1.class,Testcase2.class})
+public class TestSuite {
+}
---
+++ JunitTest/src/Testcase1.java
@@ -0,0 +1,28 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Testcase1 {
+    @Before
+    public void before(){
+        System.out.println("测试前的准备工作");
+    }
+
+    @After
+    public void after(){
+        System.out.println("测试后的处理");
+    }
+
+    @Test
+    public void testsum1(){
+        int result = SumUtil.sum1(1,2);
+        Assert.assertEquals(result,3);
+    }
+
+    @Test
+    public void testsum2(){
+        int result = SumUtil.sum2(1,2,3);
+        Assert.assertEquals(result,5);
+    }
+}
